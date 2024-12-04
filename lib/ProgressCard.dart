b770490cd8bcb56712b3ff2e7e7fc274f6360afe
Edwin <edwinnnzx@gmail.com>
2024-12-04 14:04:12 +0800

    fix progresscard asset

--- lib/ProgressCard.dart
+++ lib/ProgressCard.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Progresscard extends StatelessWidget {
@@ -11,112 +13,121 @@ class Progresscard extends StatelessWidget {
       children: [
         Container(
           height: 290,
-          width: 202,
+          width: 200,
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(12),
             color: Colors.blue,
             image: const DecorationImage(
               image: AssetImage(
-                'assets/images/Learn_Progress.png',
+                'assets/learn_progress.png',
               ),
               fit: BoxFit.cover,
             ),
           ),
-          child: Stack(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              Positioned(
-                top: 150,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: const BorderRadius.vertical(
-                      bottom: Radius.circular(12),
-                    ),
-                    gradient: LinearGradient(
-                      begin: Alignment.topCenter,
-                      end: Alignment.bottomCenter,
-                      colors: [
-                        Colors.transparent,
-                        Colors.grey.withOpacity(1),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-              // Teks Java Basics
-              Positioned(
-                bottom: 60,
-                left: 16,
-                child: Text(
-                  text,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontSize: 14,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              // Tombol Resume
-              Positioned(
-                bottom: 16,
-                left: 16,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFF151522),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 16,
-                      vertical: 8,
-                    ),
-                  ),
-                  child: const Text(
-                    "Resume",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 14,
-                    ),
-                  ),
-                ),
-              ),
-              // Circular Progress Indicator
-              Positioned(
-                bottom: 16,
-                right: 16,
-                child: Stack(
-                  alignment: Alignment.center,
+              ContainerBlurredBackground(
+                child: Row(
                   children: [
-                    SizedBox(
-                      height: 48,
-                      width: 48,
-                      child: CircularProgressIndicator(
-                        value: progress,
-                        strokeWidth: 3,
-                        backgroundColor: Colors.grey.withOpacity(0.3),
-                        valueColor: const AlwaysStoppedAnimation<Color>(
-                          Color(0xFF4AC983), // Warna progress
-                        ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            text,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 14,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 4.0,
+                          ),
+                          SizedBox(
+                            height: 20,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(0xFF151522),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(4),
+                                ),
+                                padding: const EdgeInsets.symmetric(),
+                              ),
+                              child: const Text(
+                                "Resume",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 10,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    Text(
-                      '${(progress * 100).toInt()}%', // Teks progress dalam persen
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 12,
-                      ),
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        SizedBox(
+                          height: 48,
+                          width: 48,
+                          child: CircularProgressIndicator(
+                            value: progress,
+                            strokeWidth: 3,
+                            backgroundColor: Colors.grey.withOpacity(0.3),
+                            valueColor: const AlwaysStoppedAnimation<Color>(
+                              Color(0xFF4AC983), // Warna progress
+                            ),
+                          ),
+                        ),
+                        Text(
+                          '${(progress * 100).toInt()}%', // Teks progress dalam persen
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12,
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ),
+              )
             ],
           ),
         ),
       ],
+    );
+  }
+}
+
+class ContainerBlurredBackground extends StatelessWidget {
+  const ContainerBlurredBackground({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.only(
+        bottomLeft: Radius.circular(12),
+        bottomRight: Radius.circular(12),
+      ),
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
+        decoration: BoxDecoration(color: Colors.white10),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaY: 10.0, sigmaX: 10.0),
+          child: child,
+        ),
+      ),
     );
   }
 }
