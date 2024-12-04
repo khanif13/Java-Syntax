952f503ba6c1b141c12d6bd130395ad8761c4e56
Edwin <edwinnnzx@gmail.com>
2024-12-04 14:08:46 +0800

    fix tombol asset

--- lib/Tombol.dart
+++ lib/Tombol.dart
@@ -10,14 +10,29 @@ class Tombol extends StatelessWidget {
       children: [
         Container(
           height: 50,
-          width: 50,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Image.asset(
-              'assets/images/Logo.png',
-              fit: BoxFit.cover,
-            ),
+          child: Row(
+            children: [
+              Transform.scale(
+                scale: -1,
+                child: Image.asset("assets/bracket.png"),
+              ),
+              Image.asset(
+                "assets/java.png",
+                width: 35,
+              ),
+              SizedBox(
+                width: 2.0,
+              ),
+              Image.asset("assets/bracket.png")
+            ],
           ),
+          // child: ClipRRect(
+          //   borderRadius: BorderRadius.circular(10),
+          //   child: Image.asset(
+          //     'assets/images/Logo.png',
+          //     fit: BoxFit.cover,
+          //   ),
+          // ),
         ),
         SizedBox(height: 8), // Spasi antara gambar dan teks
         Text(
